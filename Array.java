37308c53dc72b44cd139f0ba3d29e18ff10753cb
Mohsin Shaikh <testms@gmail.com>
2024-07-12 16:57:38 +0530

    Made changes in Array test cases to include reporting

---
+++ Array.java
@@ -0,0 +1,36 @@
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Define the array
+	    int[] numbers = {1, 5, 8, 2, 9};
+	    int left = 0;
+	    int right = numbers.length - 1;
+
+	    while (left < right) {
+	      // Swap elements at left and right indices
+	      int temp = numbers[left];
+	      numbers[left] = numbers[right];
+	      numbers[right] = temp;
+
+	      // Move pointers towards the middle
+	      left++;
+	      right--;
+	    }
+
+	    // Print the reversed array
+	    System.out.print("Reversed array: ");
+	    for (int num : numbers) {
+	      System.out.print(num + " ");
+	    }
+	    System.out.println();
+	  }
+	}
+	    
+	
+
+
+
+
