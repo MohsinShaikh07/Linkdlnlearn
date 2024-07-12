33b2aa27a7fac3fb2ab46be1d1de16bed4d1505e
Mohsin Shaikh <testms@gmail.com>
2024-07-12 12:28:56 +0530

    Fixed the defect #324 for ArrayList syntax

---
+++ ArrayListDemo.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class ArrayListDemo {
+
+    public static void main(String[] args) {
+
+        // Create an ArrayList of Strings
+        ArrayList<String> classmates = new ArrayList<>();
+
+        // Add elements to the ArrayList
+        classmates.add("Sabiha");
+        classmates.add("Annam");
+        classmates.add("Mohsin");
+        classmates.add("Sabiha");
+        classmates.add("Annam");
+        classmates.add("Sidra");
+        System.out.println("classmates: " + classmates);
+
+        Set<String> uniqueclassmates = new LinkedHashSet<>(classmates);
+        classmates.clear();
+        
+        classmates.addAll(uniqueclassmates);
+        
+        System.out.println("List after removing duplicates: " + classmates);
+    
+    }
+            }
+
+
